6d5413ce64a9cf28ef5d0529c307e7305725edbd
RahulNaktode <rahulnaktode04@gmail.com>
2026-05-19 20:38:50 +0530

    add Helper and GaugeCart component

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { dashboardStyles, trendStyles, chartStyles } from '../assets/dummyStyles.js'
+import {GAUGE_COLORS, COLORS, INCOME_COLORS, EXPENSE_CATEGORY_COLOR} from '../assets/color.jsx'
 
 function Dashboard() {
   return (
